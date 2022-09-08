8e61629ca98d4c5f6c304618e1a3632eb356ba53
faisalibal33 <faisaliqbal030397@gmail.com>
2022-09-08 14:25:27 +0700

    responsive navigation

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -12,55 +12,53 @@ const Nav = () => {
   const [activeNav, setActiveNav] = useState("#");
 
   return (
-    <Fade bottom>
-      <nav>
-        <Tooltip title="Me" placement="top" arrow>
-          <a
-            href="#header"
-            onClick={() => setActiveNav("#")}
-            className={activeNav === "#" ? "active" : ""}
-          >
-            <FiHome />
-          </a>
-        </Tooltip>
-        <Tooltip title="About me" placement="top" arrow>
-          <a
-            href="#about"
-            onClick={() => setActiveNav("about")}
-            className={activeNav === "about" ? "active" : ""}
-          >
-            <FiUser />
-          </a>
-        </Tooltip>
-        <Tooltip title="Experience" placement="top" arrow>
-          <a
-            href="#project"
-            onClick={() => setActiveNav("experience")}
-            className={activeNav === "experience" ? "active" : ""}
-          >
-            <BsBook />
-          </a>
-        </Tooltip>
-        <Tooltip title="Skills" placement="top" arrow>
-          <a
-            href="#services"
-            onClick={() => setActiveNav("services")}
-            className={activeNav === "services" ? "active" : ""}
-          >
-            <MdHomeRepairService />
-          </a>
-        </Tooltip>
-        <Tooltip title="Contact me" placement="top" arrow>
-          <a
-            href="#contact"
-            onClick={() => setActiveNav("contact")}
-            className={activeNav === "contact" ? "active" : ""}
-          >
-            <BiMessageSquareDetail />
-          </a>
-        </Tooltip>
-      </nav>
-    </Fade>
+    <nav>
+      <Tooltip title="Me" placement="top" arrow>
+        <a
+          href="#header"
+          onClick={() => setActiveNav("#")}
+          className={activeNav === "#" ? "active" : ""}
+        >
+          <FiHome />
+        </a>
+      </Tooltip>
+      <Tooltip title="About me" placement="top" arrow>
+        <a
+          href="#about"
+          onClick={() => setActiveNav("about")}
+          className={activeNav === "about" ? "active" : ""}
+        >
+          <FiUser />
+        </a>
+      </Tooltip>
+      <Tooltip title="Experience" placement="top" arrow>
+        <a
+          href="#project"
+          onClick={() => setActiveNav("experience")}
+          className={activeNav === "experience" ? "active" : ""}
+        >
+          <BsBook />
+        </a>
+      </Tooltip>
+      <Tooltip title="Skills" placement="top" arrow>
+        <a
+          href="#services"
+          onClick={() => setActiveNav("services")}
+          className={activeNav === "services" ? "active" : ""}
+        >
+          <MdHomeRepairService />
+        </a>
+      </Tooltip>
+      <Tooltip title="Contact me" placement="top" arrow>
+        <a
+          href="#contact"
+          onClick={() => setActiveNav("contact")}
+          className={activeNav === "contact" ? "active" : ""}
+        >
+          <BiMessageSquareDetail />
+        </a>
+      </Tooltip>
+    </nav>
   );
 };
 
